c6966c0c8f673ffff300980ed8acad494b4c4d14
Tech-Genius <78614131+Tech-Genius@users.noreply.github.com>
2022-06-21 13:03:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-var fname = "Habeeb";
-var lname = "Adebimpe"
-var height = "165cm"
-var country = "Nigeria"
-console.log("Name: " + fname + " " + lname)
-console.log("Height " + height)
-console.log("Country: " + country)
+// For Alert Prompt'
+
+let fname = prompt("Enter Your First Name:")
+let lname = prompt("Enter Your Last Name:")
+let height = prompt("What's Your Height?", "cm")
+let country = prompt("Which Country Do You Hail From?")
+alert(` Welcome ${fname} ${lname} 
+Here are your details:
+
+1. First Name: ${fname}
+
+2. Last Name: ${lname} 
+
+3. Height: ${height} tall
+
+4. Country: ${country}`)
+ 
+
+
+
+// For Console 
+
+// var fname = "Habeeb";
+// var lname = "Adebimpe"
+// var height = "165cm"
+// var country = "Nigeria"
+// console.log("Name: " + fname + " " + lname)
+// console.log("Height " + height)
+// console.log("Country: " + country)
